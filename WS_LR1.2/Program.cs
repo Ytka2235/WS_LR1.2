e9e3d04dd346efdbf4ab34318899bc7383e8be10
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2024-04-15 11:32:15 +0300

    Добавьте файлы проекта.

---
+++ WS_LR1.2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ComplexNumber num1 = new ComplexNumber(2, -10);
+        ComplexNumber num2 = new ComplexNumber(-1, 2);
+
+        Console.WriteLine($"Число 1: {num1.ToString()}");
+        Console.WriteLine($"Число 2: {num2.ToString()}");
+
+        Console.WriteLine($"Сложение: {num1.Add(num2).ToString()}");
+        Console.WriteLine($"Вычитание: {num1.Subtract(num2).ToString()}");
+        Console.WriteLine($"Умножение: {num1.Multiply(num2).ToString()}");
+        Console.WriteLine($"Деление: {num1.Divide(num2).ToString()}");
+    }
+}
+
+class ComplexNumber
+{
+    public double Real { get; set; }
+    public double Imaginary { get; set; }
+
+    public ComplexNumber(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    public ComplexNumber Add(ComplexNumber other)
+    {
+        return new ComplexNumber(Real + other.Real, Imaginary + other.Imaginary);
+    }
+
+    public ComplexNumber Subtract(ComplexNumber other)
+    {
+        return new ComplexNumber(Real - other.Real, Imaginary - other.Imaginary);
+    }
+
+    public ComplexNumber Multiply(ComplexNumber other)
+    {
+        double newReal = Real * other.Real - Imaginary * other.Imaginary;
+        double newImaginary = Imaginary * other.Real + Real * other.Imaginary;
+        return new ComplexNumber(newReal, newImaginary);
+    }
+
+    public ComplexNumber Divide(ComplexNumber other)
+    {
+        double divisor = other.Real * other.Real + other.Imaginary * other.Imaginary;
+        double newReal = (Real * other.Real + Imaginary * other.Imaginary) / divisor;
+        double newImaginary = (Imaginary * other.Real - Real * other.Imaginary) / divisor;
+        return new ComplexNumber(newReal, newImaginary);
+    }
+
+    public  string ToString()
+    {
+        return $"{Real} + {Imaginary}i";
+    }
+}
+
+
